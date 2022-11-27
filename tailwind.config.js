4a5ad907e4f6d1fb9abc32b6f0716295761065bd
tp47 <adadu@tutanota.com>
2022-11-27 15:44:33 +0400

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          button: "#1da1f2",
+          bg: {
+            dark: "#171717",
+            light: "#ffffff",
+          },
+        },
+        secondary: {
+          bg: {
+            dark: "#404040",
+            light: "#f5f5f5",
+          },
+        },
+      },
+    },
   },
   plugins: [],
   darkMode: "class",
