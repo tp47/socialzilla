628d0ae1347ce8611a63f794bc16fa3cf87e1919
tp47 <adadu@tutanota.com>
2022-12-22 23:31:16 +0400

    Add auth API and auth reducer to store

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { darkModeReducer } from "./darkMode/darkMode.slice";
+import apiSlice from "../api/api.slice";
+import authReducer from "./auth/auth.slice";
 
 const store = configureStore({
   reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authReducer,
     darkMode: darkModeReducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: import.meta.env.MODE === "development",
 });
 
 export { store };
